6266c14f9a340bf3da95851e806baddf958b87ab
TPFTMON <tpftmoon@gmail.com>
2026-03-10 10:03:15 +0100

    ex01: trying to understand what to do with consts in constructor for signing and executing a form + exceptions are added

--- ex01/Form.cpp
+++ ex01/Form.cpp
@@ -4,10 +4,27 @@
 //                         ORTHODOX BASE
 // ================================================================
 
-Form::Form(){
+Form::Form()
+ : _name("Some Unknown Form"), _isSigned(false), _signRequiredGrade(minimal_grade), _execRequiredGrade(minimal_grade)
+{
     std::cout << DEF_CONSTR_MSG << FORM_MSG;
-    
+
 }
+
+Form::Form(std::string name, int gradeRequiredToSigh, int gradeRequiredToExec)
+ : _name(name), _isSigned(false)
+{
+    std::cout << NAME_AND_GRADE_REQUIREMENTS_CONSTR_MSG << FORM_MSG;
+
+    if (gradeRequiredToSigh < maximal_grade){ // 1
+        throw Form::GradeTooHighException();
+    }
+    else if (gradeRequiredToSigh > minimal_grade){ // 150
+        throw Form::GradeTooLowException();
+    }
+    this->_signRequiredGrade = gradeRequiredToSigh;
+}
+
 
 Form::Form(const Form &to_copy){
     std::cout << COPY_CONSTR_MSG << FORM_MSG;
--- ex01/Form.hpp
+++ ex01/Form.hpp
@@ -10,6 +10,7 @@
 # define FORM_MSG "\e[0;32mForm\n\e[0m"
 
 # define DEF_CONSTR_MSG "\e[0;33mDefault Constructor\e[0m called of "
+# define NAME_AND_GRADE_REQUIREMENTS_CONSTR_MSG "\e[0;33mName with Grade Requirements Constructor\e[0m called of "
 # define COPY_CONSTR_MSG "\e[0;33mCopy Constructor\e[0m called of "
 # define COPY_ASSIGN_OP_MSG "\e[0;35mCopy assignment operator\e[0m called of "
 # define DESTR_MSG "\e[0;31mDestructor\e[0m called of "
@@ -19,6 +20,9 @@
 class Bureaucrat;
 
 class Form{
+
+    static const int minimal_grade = 150;
+    static const int maximal_grade = 1;
 
     private:
         // ... some private stuff
@@ -30,6 +34,7 @@ class Form{
     public:
         // Orthodox Canonical Form:
         Form();
+        Form(std::string name, int gradeRequiredToSigh, int gradeRequiredToExec);
         Form(const Form &to_copy);
         Form& operator=(const Form &assign);
         ~Form();
@@ -38,6 +43,16 @@ class Form{
         // ... some members
         void beSigned(const Bureaucrat& Bureaucrat);
 
+        // Exceptions:
+        class GradeTooHighException : public std::exception {
+            public:
+                virtual const char* what() const throw();
+        };
+
+        class GradeTooLowException : public std::exception {
+            public:
+                virtual const char* what() const throw();
+        };
 };
 
 
